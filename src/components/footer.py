1b8f8f3a8ffaba93ecc53efe5a0d656041f6dc51
Magan Brar <151712142+maganbrar@users.noreply.github.com>
2026-05-19 16:07:07 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    # logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
+        <p style="font-weight:bold; color:white;"> Smart Attendence System </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
@@ -14,11 +14,11 @@ def footer_home():
 
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+    # logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
+        <p style="font-weight:bold; color:black;"> Smart Attendence System </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
